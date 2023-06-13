f02e89467d0d00095ba7fa44717d4d266d92efa7
NjinuParam <njinu@webparam.org>
2023-06-13 09:19:48 +0200

    feat(update)

--- dist/index.js
+++ dist/index.js
@@ -4,14 +4,29 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+// import express from 'express';
+const mongoose_1 = __importDefault(require("mongoose"));
+const body_parser_1 = require("body-parser");
+const user_1 = require("./routes/user");
+const personnel_1 = require("./routes/personnel");
+const formData = require("express-form-data");
+const os = require("os");
 const app = (0, express_1.default)();
+const options = {
+    uploadDir: os.tmpdir(),
+    autoClean: true
+};
+app.use((0, body_parser_1.json)());
+app.use(user_1.userRouter, personnel_1.personnelRouter);
+// const app = express()
 const port = process.env.PORT || 8080;
 app.get('/', (_req, res) => {
-    return res.send('Express Typescript on Njinu');
+    return res.send('Express Typescript on Njinu222');
 });
 app.get('/ping', (_req, res) => {
     return res.send('pong 🏓');
 });
+mongoose_1.default.connect("mongodb+srv://suntecTMS:suntectms2022@cluster0.zm9cv.mongodb.net/viconet?retryWrites=true&w=majority");
 app.listen(port, () => {
     return console.log(`Server is listening on ${port}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,30 @@
 import express, { Request, Response } from 'express'
-
-const app = express()
+// import express from 'express';
+import mongoose from 'mongoose'
+import { json, urlencoded } from 'body-parser';
+import { userRouter } from './routes/user'
+import { personnelRouter } from './routes/personnel';
+const formData = require("express-form-data");
+const os = require("os");
+ const app = express()
+const options = {
+  uploadDir: os.tmpdir(),
+  autoClean: true
+};
+app.use(json())
+app.use(userRouter, personnelRouter)
+// const app = express()
 const port = process.env.PORT || 8080
 
 app.get('/', (_req: Request, res: Response) => {
-	return res.send('Express Typescript on Njinu')
+	return res.send('Express Typescript on Njinu222')
 })
 
 app.get('/ping', (_req: Request, res: Response) => {
 	return res.send('pong 🏓')
 })
+
+mongoose.connect("mongodb+srv://suntecTMS:suntectms2022@cluster0.zm9cv.mongodb.net/viconet?retryWrites=true&w=majority")
 
 app.listen(port, () => {
 	return console.log(`Server is listening on ${port}`)
